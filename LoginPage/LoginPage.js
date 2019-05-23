e9f0f12ecee0f34b46fc1bceb57b4590c092e4f9
Bharat Saraswat <bsaraswat45@gmail.com>
2019-05-23 19:20:06 +0530

    updated Login class and added methods

--- LoginPage/LoginPage.js
+++ LoginPage/LoginPage.js
@@ -1,20 +1,60 @@
 import React from "react";
 
+const db = {
+  username: "admin",
+  password: "admin"
+};
+
 class LoginPage extends React.Component {
-  Login() {
-    console.log("login called");
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.validateLogin = function(username, password) {
+      if (username === db.username && password === db.password) {
+        console.log("log in successful");
+      } else {
+        console.log("username or password is incorrect");
+      }
+    };
+
+    this.updateUsername = function() {
+      this.setState({
+        username: document.getElementById("username")
+      });
+    };
+    this.updatePassword = function() {
+      this.setState({
+        password: document.getElementById("password")
+      });
+    };
   }
 
   render() {
     return (
       <form>
-        <label for="username">Username</label>
-        <input type="username" name="username" />
         <br />
-        <label for="password">Password</label>
-        <input type="password" name="password" />
-
-        <button type="button" onClick={this.Login()} value="Submit">
+        <label htmlFor="username">Username</label>
+        <input
+          type="username"
+          name="username"
+          id="username"
+          onChange={this.updateUsername()}
+        />
+        <br />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button
+          type="button"
+          value="Submit"
+          onClick={() =>
+            this.validateLogin(this.state.username, this.state.password)
+          }
+        >
           Submit
         </button>
       </form>
